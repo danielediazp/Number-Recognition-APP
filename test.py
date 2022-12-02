ecb52a40ecbb3d182e54032ca3911b2fedb32c33
danieldiaz <danieldiazp@uri.edu>
2022-12-02 16:52:30 -0500

    Unit test for the machine learning model

--- test.py
+++ test.py
@@ -15,51 +15,142 @@ NUM_PASS = 0
 
 def test_1_0():
     """Test an image that contain the handwritten number 1."""
+    #  Load image
+    image = cv.imread("")[:, :, 0]
+    #  Convert the image to a numpy array and invert it
+    image = np.invert(np.array([image]))
+    #  Request the model prediction.
+    prediction = MODEL.predict(image)
+    assert prediction == 1
 
 
 def test_1_1():
     """Test an image that contain the handwritten number 1."""
+    #  Load image
+    image = cv.imread("")[:, :, 0]
+    #  Convert the image to a numpy array and invert it
+    image = np.invert(np.array([image]))
+    #  Request the model prediction.
+    prediction = MODEL.predict(image)
+    assert prediction == 1
 
 
 def test_2_0():
     """Test an image that contain the handwritten number 2."""
+    #  Load image
+    image = cv.imread()[:, :, 0]
+    #  Convert the image to a numpy array and invert it
+    image = np.invert(np.array([image]))
+    #  Request the model prediction.
+    prediction = MODEL.predict(image)
+    assert prediction == 1
 
 
 def test_2_1():
     """Test an image that contain the handwritten number 2."""
+    #  Load image
+    image = cv.imread()[:, :, 0]
+    #  Convert the image to a numpy array and invert it
+    image = np.invert(np.array([image]))
+    #  Request the model prediction.
+    prediction = MODEL.predict(image)
+    assert prediction == 2
 
 
 def test_3_0():
     """Test an image that contain the handwritten number 3."""
+    #  Load image
+    image = cv.imread()[:, :, 0]
+    #  Convert the image to a numpy array and invert it
+    image = np.invert(np.array([image]))
+    #  Request the model prediction.
+    prediction = MODEL.predict(image)
+    assert prediction == 3
 
 
 def test_3_1():
     """Test an image that contain the handwritten number 3."""
+    #  Load image
+    image = cv.imread()[:, :, 0]
+    #  Convert the image to a numpy array and invert it
+    image = np.invert(np.array([image]))
+    #  Request the model prediction.
+    prediction = MODEL.predict(image)
+    assert prediction == 3
 
 
 def test_4():
     """Test an image that contain the handwritten number 4."""
+    #  Load image
+    image = cv.imread()[:, :, 0]
+    #  Convert the image to a numpy array and invert it
+    image = np.invert(np.array([image]))
+    #  Request the model prediction.
+    prediction = MODEL.predict(image)
+    assert prediction == 4
 
 
 def test_5():
     """Test an image that contain the handwritten number 5."""
+    #  Load image
+    image = cv.imread()[:, :, 0]
+    #  Convert the image to a numpy array and invert it
+    image = np.invert(np.array([image]))
+    #  Request the model prediction.
+    prediction = MODEL.predict(image)
+    assert prediction == 5
 
 
 def test_6():
     """Test an image that contain the handwritten number 6."""
+    #  Load image
+    image = cv.imread()[:, :, 0]
+    #  Convert the image to a numpy array and invert it
+    image = np.invert(np.array([image]))
+    #  Request the model prediction.
+    prediction = MODEL.predict(image)
+    assert prediction == 6
 
 
 def test_7():
     """Test an image that contain the handwritten number 7."""
+    #  Load image
+    image = cv.imread()[:, :, 0]
+    #  Convert the image to a numpy array and invert it
+    image = np.invert(np.array([image]))
+    #  Request the model prediction.
+    prediction = MODEL.predict(image)
+    assert prediction == 7
 
 
 def test_8():
     """Test an image that contain the handwritten number 8."""
+    #  Load image
+    image = cv.imread()[:, :, 0]
+    #  Convert the image to a numpy array and invert it
+    image = np.invert(np.array([image]))
+    #  Request the model prediction.
+    prediction = MODEL.predict(image)
+    assert prediction == 8
 
 
 def test_9():
     """Test an image that contain the handwritten number 9."""
+    #  Load image
+    image = cv.imread()[:, :, 0]
+    #  Convert the image to a numpy array and invert it
+    image = np.invert(np.array([image]))
+    #  Request the model prediction.
+    prediction = MODEL.predict(image)
+    assert prediction == 9
 
 
 def test_10():
     """Test an image that contain the handwritten number 10."""
+    #  Load image
+    image = cv.imread()[:, :, 0]
+    #  Convert the image to a numpy array and invert it
+    image = np.invert(np.array([image]))
+    #  Request the model prediction.
+    prediction = MODEL.predict(image)
+    assert prediction == 10

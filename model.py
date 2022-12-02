3fd6886847e3e8164b92b43044fe979272294659
danieldiaz <danieldiazp@uri.edu>
2022-12-01 20:41:18 -0500

    digits_recognition model implemented

--- model.py
+++ model.py
@@ -20,6 +20,8 @@ x_train = tf.keras.utils.normalize(x_train, axis=1)
 #  Scale down the test data
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 #  Why we don't scale down the y data? Because this data are the labels
+
+
 #  Model format: Input layer, 2 hidden layers, Output layer
 #  Model type: Sequential
 model = tf.keras.models.Sequential()
@@ -30,8 +32,21 @@ model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
 #  The more neurons you add to the layer the more complicated the layer becomes.
 #  Unit "number of neurons"
 #  activation "activation function" relu = rectified linear unit activation function.
-model.add(tf.keras.layers.Dense(units=80, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(units=80, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=100, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=100, activation=tf.nn.relu))
 #  Output layer. This layers contains 10 neurons, and it uses the softmax activation function.
 #  softmax = takes the output of all the previous neurons and computes the probability.
 model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
+
+
+#  Compile the model.
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+#  Train the model
+#  Epochs = How many times the module is going to see the same data.
+#  How many times we repeat the process.
+model.fit(x_train, y_train, epochs=3)
+loss, accuracy = model.evaluate(x_test, y_test)
+model.save("digits_recognition")
+
+
+print(f"The model has an accuracy of {accuracy} with a loss of {loss}")

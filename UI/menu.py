e65c7809ce21592d41ab7b070377977ebcd80e61
danieldiaz <danieldiazp@uri.edu>
2022-12-03 18:18:15 -0500

    menu in progress

---
+++ UI/menu.py
@@ -0,0 +1,20 @@
+"""Class that define the behavior of the main menu"""
+
+import pygame
+from buttons import Button
+
+
+class MainMenu:
+    """Class desgined to represent the transition between states in the UI.
+
+    Methods:
+        __init__
+        _handle_events
+        _change_button_color
+        predict
+        about
+        update
+    """
+
+    def __init__(self) -> None:
+        pass

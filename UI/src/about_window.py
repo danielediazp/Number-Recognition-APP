01dfeec7c5380038efd6b359ef08406f0f919f15
Robert <roganesian@uri.edu>
2022-12-16 22:37:17 -0500

    __init__ method for AboutWindow.

--- UI/src/about_window.py
+++ UI/src/about_window.py
@@ -1,4 +1,10 @@
-"""Defines the transitions between Main Menu and About Window."""
+"""Defines the AboutWindow class that will be used to render the About Window.
+
+Typical example usage:
+    pygame.init()
+    about_window = AboutWindow(surface=surface)
+    about_window.update()
+"""
 
 import sys
 import pygame
@@ -10,10 +16,17 @@ class AboutWindow:
 
     Methods:
         __init__
+        _handle_events
         update
     """
-    def __init__(self):
-        return None
-
+    def __init__(self, surface: pygame.display) -> None:
+        self._surface = surface
+        self._github_button = Button("Github", (250, 500), "Visit the Github repository!")
+        self._back_button = Button("Back", (590, 560), "Go back to the main menu!")
+        self._buttons = [self._github_button, self._back_button]
+        self._background = pygame.image.load("UI/assets//MainMenu_background.png.png")
+        self._font = pygame.font.Font("UI/assets//Fonts/Oswald-Regular.ttf", 100)
+            
+    
     def update(self):
         return None

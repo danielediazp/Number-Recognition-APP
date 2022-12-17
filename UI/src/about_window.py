1d36392a97c816f5b830e1bbba630baa5aa58d24
Robert <roganesian@uri.edu>
2022-12-16 22:48:09 -0500

    handle events in AboutWindow

--- UI/src/about_window.py
+++ UI/src/about_window.py
@@ -8,8 +8,10 @@ Typical example usage:
 
 import sys
 import pygame
-from .button import Button
+import webbrowser
 
+from .button import Button
+from .screen_state import CURRENT_STATE
 
 class AboutWindow:
     """Defines the behavior of the About Window.
@@ -26,6 +28,19 @@ class AboutWindow:
         self._buttons = [self._github_button, self._back_button]
         self._background = pygame.image.load("UI/assets//MainMenu_background.png.png")
         self._font = pygame.font.Font("UI/assets//Fonts/Oswald-Regular.ttf", 100)
+        
+    def _handle_events(self) -> None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_position = pygame.mouse.get_pos()
+                if self._github_button.check_surface(mouse_position):
+                    webbrowser.open("https://github.com/danielediazp/Number-Recognition-APP")
+                elif self._back_button.check_surface(mouse_position):
+                    CURRENT_STATE.pop()
+                    CURRENT_STATE[0].update()
             
     
     def update(self):

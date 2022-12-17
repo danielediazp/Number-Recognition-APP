e2c566014e8217df71789945ba482989e042f2eb
Robert <roganesian@uri.edu>
2022-12-17 00:51:55 -0500

    File reformatting.

--- UI/src/about_window.py
+++ UI/src/about_window.py
@@ -7,12 +7,13 @@ Typical example usage:
 """
 
 import sys
-import pygame
 import webbrowser
+import pygame
 
 from .button import Button
 from .change_button_color import change_buttons_color
 from .screen_state import CURRENT_STATE
+
 
 class AboutWindow:
     """Defines the behavior of the About Window.
@@ -22,14 +23,19 @@ class AboutWindow:
         _handle_events
         update
     """
+
     def __init__(self, surface: pygame.display) -> None:
         self._surface = surface
         self._github_button = Button("GitHub", (210, 560))
         self._back_button = Button("Back", (590, 560))
         self._buttons = [self._github_button, self._back_button]
-        self._background = pygame.image.load("../Number-Recognition-APP/UI/assets/Backgrounds/MainMenu_background.png")
-        self._font = pygame.font.Font("../Number-Recognition-APP/UI/assets/Fonts/oswald/Oswald-Regular.ttf", 100)
-        
+        self._background = pygame.image.load(
+            "../Number-Recognition-APP/UI/assets/Backgrounds/MainMenu_background.png"
+        )
+        self._font = pygame.font.Font(
+            "../Number-Recognition-APP/UI/assets/Fonts/oswald/Oswald-Regular.ttf", 100
+        )
+
     def _handle_events(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -38,17 +44,19 @@ class AboutWindow:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_position = pygame.mouse.get_pos()
                 if self._github_button.check_surface(mouse_position):
-                    webbrowser.open("https://github.com/danielediazp/Number-Recognition-APP")
+                    webbrowser.open(
+                        "https://github.com/danielediazp/Number-Recognition-APP"
+                    )
                 elif self._back_button.check_surface(mouse_position):
                     CURRENT_STATE.pop()
                     CURRENT_STATE[0].update()
-    
+
     def update(self):
         pygame.display.set_caption("About")
         title_text = self._font.render("About", True, "#39FF14")
-        
+
         while True:
-            self._surface.blit(self._background, (0,0))
+            self._surface.blit(self._background, (0, 0))
             self._surface.blit(title_text, (93, 45))
             self._github_button.update(self._surface)
             self._back_button.update(self._surface)

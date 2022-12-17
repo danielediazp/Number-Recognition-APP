4ae88128a9ebdcc277c0357830eea389041670a3
Robert <roganesian@uri.edu>
2022-12-17 05:44:27 -0500

    Pylint warning disables.

--- UI/src/about_window.py
+++ UI/src/about_window.py
@@ -15,6 +15,8 @@ from .change_button_color import change_buttons_color
 from .screen_state import CURRENT_STATE
 
 
+#  pylint: disable=locally-disabled, no-member
+#  pylint: disable=locally-disabled, too-few-public-methods
 class AboutWindow:
     """Defines the behavior of the About Window.
 
